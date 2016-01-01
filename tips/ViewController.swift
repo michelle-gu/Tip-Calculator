55916b5939ca8f65eb242d58f5f1ef2863eec0f9
Doris Chan <Doris@Doriss-MacBook-Air.local>
2015-12-31 20:06:36 -0600

    Basic tip calculator

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  tips
 //
-//  Created by Doris Chan on 12/31/15.
+//  Created by Michelle Gu on 12/31/15.
 //  Copyright © 2015 michelle-gu. All rights reserved.
 //
 
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingCHanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+    
+        let billAmount = (billField.text! as NSString).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
